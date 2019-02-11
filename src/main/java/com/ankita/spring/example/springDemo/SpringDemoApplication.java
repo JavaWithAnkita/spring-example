c3fe5e74b2ed867d9e7249115447b9089dd67e87
JavaWithAnkita <47524608+JavaWithAnkita@users.noreply.github.com>
2019-02-11 18:29:03 +0530

    Update SpringDemoApplication.java

--- src/main/java/com/ankita/spring/example/springDemo/SpringDemoApplication.java
+++ src/main/java/com/ankita/spring/example/springDemo/SpringDemoApplication.java
@@ -2,6 +2,9 @@ package com.ankita.spring.example.springDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 @RestController
