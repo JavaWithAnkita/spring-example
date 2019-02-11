43f0fff6e657821778847f8eaa16b7e2d33a89f8
JavaWithAnkita <47524608+JavaWithAnkita@users.noreply.github.com>
2019-02-11 17:32:25 +0530

    Add files via upload

---
+++ src/main/java/com/ankita/spring/example/springDemo/SpringDemoApplication.java
@@ -0,0 +1,25 @@
+package com.ankita.spring.example.springDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String welcome(){
+		return "Welcome to openshift"
+	}
+
+	@GetMapping("/{input}")
+			public String congrats(@PathVariable String input){
+				return "Congrats" + input + "U have successfully deployed application on Openshift"
+			}
+
+}
+
